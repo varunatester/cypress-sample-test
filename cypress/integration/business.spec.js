78dd25f4d85dcee8a3f58f46d6736a48e0e5b77c
Varuna Srivastava <srivastavavaruna@gmail.com>
2020-01-17 19:29:27 -0500

    Added a two sample tests

---
+++ cypress/integration/business.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+describe('business', () => {
+
+  beforeEach(function() {
+  
+  })
+
+  it('business', async () => {
+   cy.visit("http://www.google.com")
+  })
+})
---
+++ cypress/integration/consumer.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+describe('consumer', () => {
+
+    beforeEach(function() {
+    
+    })
+  
+    it('consumer', async () => {
+     cy.visit("http://www.google.com")
+    })
+  })
+  
